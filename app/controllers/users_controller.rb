c7bcda67606e8ee115d24ac5f25d7391f0dd6be2
dDiego-g <diego_a981@live.com.mx>
2020-05-10 23:35:27 -0500

    inicio sesion funcional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,16 @@ class UsersController < ApplicationController
         @user = User.new
     end
     def create
-    end
+        @user = User.new(user_params)
+        if @user.save
+         flash[:success] = "Bienvenido a Dibber #{@user.username}"
+         redirect_to posts_path
+        else
+         render 'new'
+        end
+       end
+    private
+def user_params
+  params.require(:user).permit(:username, :email, :password)
+end
     end
